f1cc25528b8fbde9270ee5abcbb4870a0bfeaed9
Mary Garcia <rose.pajanel@yahoo.com>
2024-08-18 21:56:30 +1200

    project set-up

---
+++ backend/src/index.tsx
@@ -0,0 +1,32 @@
+import express from 'express';
+import * as admin from 'firebase-admin';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+
+var serviceAccount = require('../serviceAccountKey.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+});
+
+const db = admin.database();
+
+// Middleware
+// app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Encrypto-Chat');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running http://localhost:${port}`);
+});
