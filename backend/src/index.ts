934b22eb69848c4dfc361816baf3368942ec4ddc
Mary Garcia <rose.pajanel@yahoo.com>
2024-08-20 13:18:55 +1200

    gitignore fixed path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ var serviceAccount = require('../serviceAccountKey.json')
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 });
 
 const db = admin.database();
