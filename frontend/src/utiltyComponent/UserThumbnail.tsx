c925d1c4296c878426929c81a728b40655311d91
Matthew Nolan <mnolan132@gmail.com>
2024-10-31 10:59:28 +1300

    UserThumbnail now has a random color as its background

--- frontend/src/utiltyComponent/UserThumbnail.tsx
+++ frontend/src/utiltyComponent/UserThumbnail.tsx
@@ -18,17 +18,39 @@ const UserThumbnail: React.FC<UserThumbnailProps> = ({
   const getInitials = (firstName: string, lastName: string) =>
     `${firstName[0] || ""}${lastName[0] || ""}`.toUpperCase();
 
+  const backgroundColorsArray = [
+    "#7cf494",
+    "#6f97e2",
+    "#f7b2bc",
+    "#f9d8a7",
+    "#d8f796",
+    "#998bf4",
+    "#ddb8f9",
+    "#fcc2e5",
+    "#e8ffb2",
+  ];
+
+  const randomBackgroundColor = (backgroundColorsArray: string[]) => {
+    const randomIndex = Math.floor(
+      Math.random() * backgroundColorsArray.length
+    );
+    return backgroundColorsArray[randomIndex];
+  };
+
+  const backgroundColor = randomBackgroundColor(backgroundColorsArray);
+
   return (
     <Flex
       width={diameter}
       height={diameter}
       borderRadius="full"
-      bg="gray.300"
+      bg={backgroundColor}
       align="center"
       justify="center"
       fontSize={fontSize}
       fontWeight="bold"
-      color="white"
+      color="black"
+      opacity={"0.2"}
     >
       {getInitials(firstName || "", lastName || "")}
     </Flex>
